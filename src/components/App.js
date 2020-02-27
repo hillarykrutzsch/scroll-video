3cde84c5caf2b1e7add5fda65a240d65be77afff
Hillary <hillary.krutzsch@gmail.com>
2020-02-27 12:47:03 -0800

    fixing imports for addIndicator function

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import "../styles/App.scss";
 import * as ScrollMagic from "scrollmagic"; // Or use scrollmagic-with-ssr to avoid server rendering problems
 import { TweenMax, TimelineMax, gsap } from "gsap"; // Also works with TweenLite and TimelineLite
 import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
+import "scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators";
 
 ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
 
