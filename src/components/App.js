2b1f056289bf0406850957f6ccc7bba189db1c8f
Hillary <hillary.krutzsch@gmail.com>
2020-02-28 11:39:28 -0800

    change class names, add parallax image and gradient sections

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,85 @@
 import React, { useRef, useEffect } from "react";
 import "../styles/App.scss";
 import * as ScrollMagic from "scrollmagic"; // Or use scrollmagic-with-ssr to avoid server rendering problems
-import { TweenMax, TimelineMax, gsap } from "gsap"; // Also works with TweenLite and TimelineLite
+import { TweenMax, TimelineMax, gsap, Power1 } from "gsap"; // Also works with TweenLite and TimelineLite
 import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
-import "scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators";
+//import "scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators";
 
 ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
 
 const App = () => {
-  const first = useRef(null);
-  const second = useRef(null);
-  const third = useRef(null);
+  const scrollVid = useRef(null);
+  const parallax = useRef(null);
   const video = useRef(null);
   const h1text = useRef(null);
+  const background = useRef(null);
 
   useEffect(() => {
     let controller = new ScrollMagic.Controller();
-    let scene1 = new ScrollMagic.Scene({
-      duration: 2000,
-      triggerElement: first.current,
-      triggerHook: 0
-    })
-      .addIndicators()
-      .setPin(first.current)
-      .addTo(controller);
-
-    let textAnim = gsap.fromTo(
-      h1text.current,
-      1,
-      { opacity: 1 },
-      { opacity: 0 }
-    );
-
-    let scene2 = new ScrollMagic.Scene({
-      duration: 1000,
-      offset: 200,
-      triggerElement: first.current,
-      triggerHook: 0
-    })
-      .setTween(textAnim)
-      .addTo(controller);
 
     let accelAmount = 0.5;
     let scrollPos = 0;
     let delay = 0;
 
+    //SCENE 1 Video
+    let scene1 = new ScrollMagic.Scene({
+      duration: 2000,
+      triggerElement: scrollVid.current,
+      triggerHook: 0
+    })
+      // .addIndicators()
+      .setPin(scrollVid.current)
+      .addTo(controller);
+
     scene1.on("update", e => {
       scrollPos = e.scrollPos / 1000;
     });
-
     setInterval(() => {
       delay += (scrollPos - delay) * accelAmount;
       video.current.currentTime = delay;
     }, 166);
+
+    //SCENE 2 TEXT ANIMATION
+    let scene2 = new ScrollMagic.Scene({
+      duration: 1000,
+      offset: 200,
+      triggerElement: scrollVid.current,
+      triggerHook: 0
+    })
+      .setTween(gsap.fromTo(h1text.current, 1, { opacity: 1 }, { opacity: 0 }))
+      .addTo(controller);
+
+    //SCENE 3 PARALLAX
+    var scene3 = new ScrollMagic.Scene({
+      triggerElement: parallax.current,
+      triggerHook: 1,
+      duration: "100%"
+    })
+      .setTween(
+        gsap.from(background.current, 1, {
+          y: "-20%",
+          autoAlpha: 0.3,
+          ease: Power1.easeInOut
+        })
+      )
+      .addTo(controller);
   }, []);
 
   return (
     <div>
-      <section ref={first} className="first">
-        <h1 ref={h1text}>The Big Title of the Page</h1>
-        <video ref={video} src="/video/IMG_0763.mp4"></video>
+      <section ref={scrollVid} className="scroll-vid">
+        <h1 ref={h1text}>Here is a scrolling video</h1>
+        <video ref={video} src="/media/IMG_0763.mp4"></video>
       </section>
-      <section ref={second} className="second">
+      <section className="gradient">
+        <h1>A beautiful gradient</h1>
+      </section>
+      <section ref={parallax} className="parallax">
+        <div ref={background} className="background"></div>
         <h1>The Second Section</h1>
       </section>
-      <section ref={third} className="third">
-        <h1>The Third Section</h1>
+      <section className="gradient">
+        <h1>Another beautiful gradient</h1>
       </section>
     </div>
   );
